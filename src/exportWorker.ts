569a66998c300788f8080d8b54fdaed45b485835
wenzhe <mysteryven@gmail.com>
2022-08-17 21:02:22 +0800

    fix: spread

--- src/exportWorker.ts
+++ src/exportWorker.ts
@@ -3,7 +3,7 @@ import { WORKER_STATUS } from './useWorker'
 export default function exportWorker<T extends (...args: any) => any>(fn: T) {
     self.onmessage = async function ({ data }) {
         try {
-            (self.postMessage)([WORKER_STATUS.SUCCESS, await fn(data)])
+            (self.postMessage)([WORKER_STATUS.SUCCESS, await fn(...data)])
         } catch (e) {
             self.postMessage([WORKER_STATUS.ERROR, e])
         }
