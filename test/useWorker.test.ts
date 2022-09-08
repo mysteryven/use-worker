3fb87870347c8a9a338beb5261e5d51d09c1866b
wenzhe <mysteryven@gmail.com>
2022-09-08 21:27:28 +0800

    chore: remove defineWebWorker

--- test/useWorker.test.ts
+++ test/useWorker.test.ts
@@ -1,13 +1,11 @@
 import { afterEach, beforeEach, describe, expect, it } from 'vitest'
 import { renderHook } from '@testing-library/react-hooks'
 import useWorker, { WORKER_STATUS } from '../src/useWorker'
-import { defineWebWorker } from './worker.mock'
 
 describe('useWorker', () => {
     let createWorker: () => Worker
 
     beforeEach(() => {
-        defineWebWorker()
 
         createWorker = () => {
             return new Worker(new URL('./fibonacci.worker', import.meta.url), {
@@ -16,16 +14,18 @@ describe('useWorker', () => {
         }
     })
 
-    afterEach(() => {
-        // @ts-ignore
-        delete globalThis.Worker
-    })
-
     it('should be defined', () => {
         expect(useWorker).toBeDefined()
     })
 
     it('should post message to web worker', async () => {
+        const { result } = renderHook(() => useWorker(createWorker))
+        const value = await result.current.workerRunner(5)
+        expect(value).toStrictEqual(8)
+    })
+
+
+    it('should post message to web worker2', async () => {
         const { result } = renderHook(() => useWorker(createWorker))
         const value = await result.current.workerRunner(5)
         expect(value).toStrictEqual(8)
